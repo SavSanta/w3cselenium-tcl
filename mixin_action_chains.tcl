b2e38410cc9aafc95bcb1a8dfca465d5ac969f09
SavSanta <remethyst@yahoo.com>
2019-11-20 10:24:17 -0500

    Several mixin_action modifications
    Namespace variables added for proper dispatching.
    Moved most of the w3c WebDriver new style actions into this file.
    Removed lead "actions:" key from json body. As it gets munged in later by the executor and all we need is the json array portion.
    Sucessfully tested and implemented a few w3c WD mouse actions and the "json" body results on Wireshark.

--- mixin_action_chains.tcl
+++ mixin_action_chains.tcl
@@ -1,54 +1,58 @@
 namespace eval ::selenium {
 
 	oo::class create Mixin_Action_Chains {
-
+		# Making already set variables from higher scope available here
+        variable Mouse_Button Command session_ID
+		# Setting some variables in this particular namespace
 		variable action_list {}
+		# Cant be seen. Fix it
 		variable duration 150
 
 
-		method perform {} {
+		method w3c_perform {} {
 
 			}
 
-		method reset_actions {} {
+		method w3c_reset_actions {} {
 
 			my execute $Command(W3C_RELEASE_ACTIONS) sessionId $session_ID
 
 		}
 
-		method click {{element_ID ""}} {
-			if {element_ID ne ""} {
-				my move_to_element $element_ID
-			}
+		method w3c_click {{element_ID ""}} {
+			if {$element_ID ne ""} {
+				my w3c_move_to_element $element_ID
+			} 
 
 
-			 set bex {
-			   "actions": [
+			set action_payload {
+				[
 				 {
 				   "type": "pointer",
 				   "id": "finger1",
 				   "parameters": {"pointerType": "mouse"},
 				   "actions": [
 					 {"type": "pointerDown", "button": 0},
-					 {"type": "pause", "duration": 200},
+					 {"type": "pause", "duration": 150},
 					 {"type": "pointerUp", "button": 0}
 				   ]
 				 }
-				 ]
+				]
 			 }
-
-
+			
+			my execute $Command(W3C_PERFORM_ACTIONS) sessionId $session_ID actions $action_payload
+            
 		}
 
-		method click_and_hold {{element_ID ""}} {
 
-			if {element_ID ne ""} {
-				my move_to_element $element_ID
+		method w3c_click_and_hold {{element_ID ""}} {
+
+			if {$element_ID ne ""} {
+				my w3c_move_to_element $element_ID
 			}
 
-
-			 set bex {
-			   "actions": [
+			 set action_payload {
+				[
 				 {
 				   "type": "pointer",
 				   "id": "finger1",
@@ -57,22 +61,23 @@ namespace eval ::selenium {
 					 {"type": "pointerDown", "button": 0}
 				   ]
 				 }
-				 ]
+				]
 			 }
 
+			my execute $Command(W3C_PERFORM_ACTIONS) sessionId $session_ID actions $action_payload
 
 		}
 
 
-		method context_click {{element_ID ""}} {
+		method w3c_context_click {{element_ID ""}} {
 
-			if {element_ID ne ""} {
-				my move_to_element $element_ID
+			if {$element_ID ne ""} {
+				my w3c_move_to_element $element_ID
 			}
 
 
-			 set bex {
-			   "actions": [
+			 set action_payload {
+				[
 				 {
 				   "type": "pointer",
 				   "id": "finger1",
@@ -83,22 +88,22 @@ namespace eval ::selenium {
 					 {"type": "pointerUp", "button": 2}
 				   ]
 				 }
-				 ]
+				]
 			 }
 
+			my execute $Command(W3C_PERFORM_ACTIONS) sessionId $session_ID actions $action_payload
+			
 
 		}
 
-		method double_click {{element_ID ""}} {
+		method w3c_double_click {{element_ID ""}} {
 
-			if {element_ID ne ""} {
-				my move_to_element $element_ID
+			if {$element_ID ne ""} {
+				my w3c_move_to_element $element_ID
 			}
 
-
-
-			 set bex {
-			   "actions": [
+			 set action_payload {
+				[
 				 {
 				   "type": "pointer",
 				   "id": "finger1",
@@ -115,56 +120,81 @@ namespace eval ::selenium {
 				 }
 			   ]
 			 }
+			 
+			my execute $Command(W3C_PERFORM_ACTIONS) sessionId $session_ID actions $action_payload
 		}
 
-		method drag_and_drop { element_start, element_end } {
+		method w3c_drag_and_drop { element_start, element_end } {
 
-			my click_and_hold {element_start}
-			my move_to_element {element_end}
-			my release {}
+			my w3c_click_and_hold $element_start
+			my w3c_move_to_element $element_end
+			my w3c_release {}
 			return
 
 			}
 
 
-
 		#method drag_and_drop_by_offset {source, xoffset, yoffset }
 		#method move_by_offset {self, xoffset, yoffset }
 
-		method move_to_element {element_ID } {
-			if {element_ID eq ""} {
+		method w3c_move_to_element {element_ID} {
+			variable duration 
+			if {$element_ID eq ""} {
 				throw {Missing Element} {Error: Element ID Must Be Supplied}
 			}
 
-			my execute $Command(W3C_GET_ELEMENT_RECT) sessionId $session_ID id $element_ID]
-			set element_rect_x [dict get element_rect width]
-			set element_rect_y [dict get element_rect height]
+			set element_rect [my execute $Command(W3C_GET_ELEMENT_RECT) sessionId $session_ID id $element_ID]
+			set element_rect_x [expr int( [dict get $element_rect value width] )]
+			set element_rect_y [expr int( [dict get $element_rect value height] )]
 
-			set left_offset [expr element_rect_x / 2 ]
-			set top_offset [expr element_rect_y / 2 ]
-			set xcoord [set expr -left_offset + { element_rect_x | 0} ]
-			set ycoord [set expr -top_offset + { element_rect_y | 0} ]
+			set left_offset [expr $element_rect_x / 2 ]
+			set top_offset [expr $element_rect_y / 2 ]
+
+			set xcoord [expr -$left_offset + { $element_rect_x | 0} ]
+			set ycoord [expr -$top_offset + { $element_rect_y | 0} ]
 
 			set action_payload "
-				{
-			   \"actions\": \[
+				
+				\[
 				 {
 				   \"type\": \"pointer\",
-				   \"id\": \"finger1\",
+				   \"id\": \"mouse1\",
 				   \"parameters\": {\"pointerType\": \"mouse\"},
 				   \"actions\": \[
-					 {\"type\": \"pointerMove\", \"duration\": $duration, \"x\": xcoord, \"y\": ycoord}
+					 {\"type\": \"pointerMove\", \"duration\": $duration, \"x\": $xcoord, \"y\": $ycoord}
 				   \]
 				 }
-						\]
-			}
+				 \]
+			
 			"
 
+			my execute $Command(W3C_PERFORM_ACTIONS) sessionId $session_ID actions $action_payload
 
 		}
 
 
-		method release {} {
+		 ##This is still trash and I need to review and then fix it
+		#method w3c_move_to_element_with_offset {element_ID, xoffset, yoffset } {
+
+			#if {$element_ID ne ""} {
+
+				#set element_rect [my execute $Command(W3C_GET_ELEMENT_RECT) sessionId $session_ID id $element_ID]]
+				#set element_rect_x [dict get $element_rect width]
+				#set element_rect_y [dict get $element_rect height]
+
+				#set left_offset [expr $element_rect_x / 2 ]
+				#set top_offset [expr $element_rect_y / 2 ]
+				#set left [set expr -$left_offset + { $element_rect_x | 0} ]
+				#set top [set expr -$top_offset + { $element_rect_y | 0} ]
+			#} else {
+				#set left 0
+				#set top 0
+			#}
+
+		#}
+		
+		
+		method w3c_release {} {
 
 			my execute $Command(W3C_RELEASE_ACTIONS) sessionId $session_ID
 
@@ -190,31 +220,7 @@ namespace eval ::selenium {
 
 
 
-
-		#This is complete trash and I need to fix it
-		#method move_to_element_with_offset {element_ID, xoffset, yoffset } {
-
-			#if {0} {
-			#my execute $Command(W3C_GET_ELEMENT_RECT) sessionId $session_ID id $element_ID]
-			#set element_rect_x [dict get element_rect width]
-			#set element_rect_y [dict get element_rect height]
-
-			#set left_offset [expr element_rect_x / 2 ]
-			#set top_offset [expr element_rect_y / 2 ]
-			#set left [set expr -left_offset + { element_rect_x | 0} ]
-			#set top [set expr -top_offset + { element_rect_y | 0} ]
-			#} else {
-				#set left 0
-				#set top 0
-			#}
-
-
-
-		#}
-
-
-
-		#method pause {seconds} {
+		#method w3c_pause {seconds} {
 
 			#}
 
