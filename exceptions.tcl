df7237b9667b38684779942277b5fca6f3d8ed8e
SavSanta <remethyst@yahoo.com>
2019-11-22 12:13:06 -0500

    Merge out duplicate Exceptions from OLD and W3C
    Since Exceptions defined changed in W3C. We can add the new ones that arent covered and remove the old ones with the same name. While now the array has mostly uncovered that are no longer needed in W3C. It doesnt hurt, so we will leave it in. Though if wish to define only W3C Exceptions we can replace the entire array contents (except WebDriverException probably) with the commented out array that was in the last commit

--- exceptions.tcl
+++ exceptions.tcl
@@ -33,29 +33,6 @@ namespace eval ::selenium {
 			Thrown when frame or window target to be switched doesn't exist.
 			}
 		}
-		
-		NoSuchElement { SELENIUM NoSuchElement {
-			Thrown when element could not be found.
-			
-			If you encounter this exception, you may want to check the following:
-			* Check your selector used in your find_by...
-			* Element may not yet be on the screen at the time of the find operation,
-			(webpage is still loading) see selenium.webdriver.support.wait.WebDriverWait()
-			for how to write a wait wrapper to wait for an element to appear.
-			}
-		}
-
-		NoSuchFrame { SELENIUM NoSuchFrame {
-			Thrown when frame target to be switched doesn't exist.
-			}
-		}
-		
-		UnknownCommand { SELENIUM UnknownCommand {
-			The requested resource could not be found, or a request was received using an HTTP method that is not
-			supported by the mapped resource.
-			}
-		}
-		
 
 		NoSuchAttribute { SELENIUM NoSuchAttribute {
 			Thrown when the attribute of element could not be found.
@@ -66,41 +43,12 @@ namespace eval ::selenium {
 			}
 		}
 
-		StaleElementReference { SELENIUM StaleElementReference {
-			Thrown when a reference to an element is now "stale".
-			
-			Stale means the element no longer appears on the DOM of the page.
-			
-			
-			Possible causes of StaleElementReferenceException include, but not limited to:
-			* You are no longer on the same page, or the page may have refreshed since the element was located.
-			* The element may have been removed and re-added to the screen, since it was located.
-			Such as an element being relocated.
-			This can happen typically with a javascript framework when values are updated and the node is rebuilt.
-			* Element may have been inside an iframe or another context which was refreshed.
-			}
-		}
-
 		ElementNotVisible { SELENIUM ElementNotVisible {
 			Thrown when an element is present on the DOM, but
 			it is not visible, and so is not able to be interacted with.
 			
 			Most commonly encountered when trying to click or read text
 			of an element that is hidden from view.
-			}
-		}
-
-
-		InvalidElementState { SELENIUM InvalidElementState {
-			Thrown when an unexpected alert is appeared.
-			
-			Usually raised when when an expected modal is blocking webdriver form executing any
-			more commands.
-			}
-		}
-
-		UnknownError { SELENIUM UnknownError {
-			 An unknown server-side error occurred while processing the command. 
 			}
 		}
 		
@@ -119,48 +67,8 @@ namespace eval ::selenium {
 			}
 		}
 		
-		Timeout { SELENIUM Timeout {
-			Thrown when a command does not complete in enough time.
-			}
-		}
-		
-		NoSuchWindow { SELENIUM NoSuchWindow {
-			Thrown when window target to be switched doesn't exist.
-			
-			To find the current set of active window handles, you can get a list
-			of the active window handles in the following way::
-			
-			print driver.window_handles
-			}
-		}
-
-
-		InvalidCookieDomain { SELENIUM InvalidCookieDomain {
-			Thrown when attempting to add a cookie under a different domain
-			than the current URL.
-			}
-		}
-
-		UnableToSetCookie { SELENIUM UnableToSetCookie {
-			Thrown when a driver fails to set a cookie.
-			}
-		}
-
-		UnexpectedAlertOpen { SELENIUM UnexpectedAlertOpen {
-			Thrown when an unexpected alert is appeared.
-    
-			Usually raised when when an expected modal is blocking webdriver form executing any 
-			more commands.
-			}
-		}
-		
 		NoAlertOpen { SELENIUM NoAlertOpen {
 			An attempt was made to operate on a modal dialog when one was not open. 
-			}
-		}
-		
-		ScriptTimeout { SELENIUM ScriptTimeout {
-			A script did not complete before its timeout expired. 
 			}
 		}
 		
@@ -169,15 +77,6 @@ namespace eval ::selenium {
 			}
 		 }		 
 		
-
-		ElementNotSelectable { SELENIUM ElementNotSelectable {
-			Thrown when trying to select an unselectable element.
-			
-			For example, selecting a 'script' element.
-			}
-		}
-
-
 		UnexpectedTagName { SELENIUM UnexpectedTagName {
 			Thrown when a support class did not get an expected web element.
 			}
@@ -194,26 +93,6 @@ namespace eval ::selenium {
 			Thrown when activating an IME engine has failed.} 
 		}
 		
-		InvalidSelector { SELENIUM InvalidSelector {
-			Thrown when the selector which is used to find an element does not return
-			a WebElement. Currently this only happens when the selector is an xpath
-			expression and it is either syntactically invalid (i.e. it is not a
-			xpath expression) or the expression does not select WebElements
-			(e.g. "count(//input)").
-			}
-		}
-
-		SessionNotCreated { SELENIUM SessionNotCreated {
-			A new session could not be created. 
-			}
-		}
-		
-		
-		MoveTargetOutOfBounds { SELENIUM MoveTargetOutOfBounds {
-			Thrown when the target provided to the `ActionsChains` move
-			method is invalid, i.e. out of document.
-			}
-		}
 		
 		InvalidCommandMethod { SELENIUM InvalidCommandMethod {
 			If a request path maps to a valid resource, but that resource does not respond to the request method,
@@ -228,6 +107,146 @@ namespace eval ::selenium {
 			The response body should list the missing parameters. 	
 			}
 		}
+
+		ElementClickIntercepted { SELENIUM ElementClickIntercepted {
+			The Element Click command could not be completed because the element receiving the events is obscuring the element that was requested clicked.
+			}
+		}
+
+		ElementNotSelectable { SELENIUM ElementNotSelectable {
+			An attempt was made to select an element that cannot be selected.
+			}
+		}
+
+		ElementNotInteractable { SELENIUM ElementNotInteractable {
+			A command could not be completed because the element is not pointer- or keyboard interactable.
+			}
+		}
+
+		InsecureCertificate { SELENIUM InsecureCertificate {
+			caused the user agent to hit a certificate warning, which is usually the result of an expired or invalid TLS certificate.
+			}
+		}
+
+		InvalidArgument { SELENIUM InvalidArgument {
+			The arguments passed to a command are either invalid or malformed.
+			}
+		}
+
+		InvalidCookieDomain { SELENIUM InvalidCookieDomain {
+			An illegal attempt was made to set a cookie under a different domain than the current page.
+			}
+		}
+
+		InvalidCoordinates { SELENIUM InvalidCoordinates {
+			The coordinates provided to an interactions operation are invalid.
+			}
+		}
+
+		InvalidElementState { SELENIUM InvalidElementState {
+			A command could not be completed because the element is in an invalid state, e.g. attempting to click an element that is no longer attached to the document.
+			}
+		}
+
+		InvalidSelector { SELENIUM InvalidSelector {
+			Argument was an invalid selector.
+			}
+		}
+
+		InvalidSessionId { SELENIUM InvalidSessionId {
+			Occurs if the given session id is not in the list of active sessions, meaning the session either does not exist or that it’s not active.
+			}
+		}
+
+		JavascriptError { SELENIUM JavascriptError {
+			An error occurred while executing JavaScript supplied by the user.
+			}
+		}
+
+		MoveTargetOutOfBounds { SELENIUM MoveTargetOutOfBounds {
+			The target for mouse interaction is not in the browser’s viewport and cannot be brought into that viewport.
+			}
+		}
+
+		NoSuchAlert { SELENIUM NoSuchAlert {
+			An attempt was made to operate on a modal dialog when one was not open.
+			}
+		}
+
+		NoSuchCookie { SELENIUM NoSuchCookie {
+			No cookie matching the given path name was found amongst the associated cookies of the current browsing context’s active document.
+			}
+		}
+
+		NoSuchElement { SELENIUM NoSuchElement {
+			An element could not be located on the page using the given search parameters.
+			}
+		}
+
+		NoSuchFrame { SELENIUM NoSuchFrame {
+			A command to switch to a frame could not be satisfied because the frame could not be found.
+			}
+		}
+
+		NoSuchWindow { SELENIUM NoSuchWindow {
+			A command to switch to a window could not be satisfied because the window could not be found.
+			}
+		}
+
+		ScriptTimeout { SELENIUM ScriptTimeout {
+			A script did not complete before its timeout expired.
+			}
+		}
+
+		SessionNotCreated { SELENIUM SessionNotCreated {
+			A new session could not be created.
+			}
+		}
+
+		StaleElementReference { SELENIUM StaleElementReference {
+			A command failed because the referenced element is no longer attached to the DOM.
+			}
+		}
+
+		Timeout { SELENIUM Timeout {
+			An operation did not complete before its timeout expired.
+			}
+		}
+
+		UnableToSetCookie { SELENIUM UnableToSetCookie {
+			A command to set a cookie’s value could not be satisfied.
+			}
+		}
+
+		UnableToCaptureScreen { SELENIUM UnableToCaptureScreen {
+			A screen capture was made impossible.
+			}
+		}
+
+		UnexpectedAlertOpen { SELENIUM UnexpectedAlertOpen {
+			A modal dialog was open, blocking this operation.
+			}
+		}
+
+		UnknownCommand { SELENIUM UnknownCommand {
+			A command could not be executed because the remote end is not aware of it.
+			}
+		}
+
+		UnknownError { SELENIUM UnknownError {
+			An unknown error occurred in the remote end while processing the command.
+			}
+		}
+
+		UnknownMethod { SELENIUM UnknownMethod {
+			The requested command matched a known URL but did not match an method for that URL.
+			}
+		}
+
+		UnsupportedOperation { SELENIUM UnsupportedOperation {
+			Indicates that a command that should have executed properly cannot be supported for some reason.
+			}
+		}
 	}
 	
 	proc Exception {exceptionName} {
@@ -235,146 +254,3 @@ namespace eval ::selenium {
 		return $Exception($exceptionName)
 	}
 }
-
-
-
-		#~ ElementClickIntercepted { SELENIUM ElementClickIntercepted {
-			#~ The Element Click command could not be completed because the element receiving the events is obscuring the element that was requested clicked.
-			#~ }
-		#~ }
-
-		#~ ElementNotSelectable { SELENIUM ElementNotSelectable {
-			#~ An attempt was made to select an element that cannot be selected.
-			#~ }
-		#~ }
-
-		#~ ElementNotInteractable { SELENIUM ElementNotInteractable {
-			#~ A command could not be completed because the element is not pointer- or keyboard interactable.
-			#~ }
-		#~ }
-
-		#~ InsecureCertificate { SELENIUM InsecureCertificate {
-			#~ caused the user agent to hit a certificate warning, which is usually the result of an expired or invalid TLS certificate.
-			#~ }
-		#~ }
-
-		#~ InvalidArgument { SELENIUM InvalidArgument {
-			#~ The arguments passed to a command are either invalid or malformed.
-			#~ }
-		#~ }
-
-		#~ InvalidCookieDomain { SELENIUM InvalidCookieDomain {
-			#~ An illegal attempt was made to set a cookie under a different domain than the current page.
-			#~ }
-		#~ }
-
-		#~ InvalidCoordinates { SELENIUM InvalidCoordinates {
-			#~ The coordinates provided to an interactions operation are invalid.
-			#~ }
-		#~ }
-
-		#~ InvalidElementState { SELENIUM InvalidElementState {
-			#~ A command could not be completed because the element is in an invalid state, e.g. attempting to click an element that is no longer attached to the document.
-			#~ }
-		#~ }
-
-		#~ InvalidSelector { SELENIUM InvalidSelector {
-			#~ Argument was an invalid selector.
-			#~ }
-		#~ }
-
-		#~ InvalidSessionId { SELENIUM InvalidSessionId {
-			#~ Occurs if the given session id is not in the list of active sessions, meaning the session either does not exist or that it’s not active.
-			#~ }
-		#~ }
-
-		#~ JavascriptError { SELENIUM JavascriptError {
-			#~ An error occurred while executing JavaScript supplied by the user.
-			#~ }
-		#~ }
-
-		#~ MoveTargetOutOfBounds { SELENIUM MoveTargetOutOfBounds {
-			#~ The target for mouse interaction is not in the browser’s viewport and cannot be brought into that viewport.
-			#~ }
-		#~ }
-
-		#~ NoSuchAlert { SELENIUM NoSuchAlert {
-			#~ An attempt was made to operate on a modal dialog when one was not open.
-			#~ }
-		#~ }
-
-		#~ NoSuchCookie { SELENIUM NoSuchCookie {
-			#~ No cookie matching the given path name was found amongst the associated cookies of the current browsing context’s active document.
-			#~ }
-		#~ }
-
-		#~ NoSuchElement { SELENIUM NoSuchElement {
-			#~ An element could not be located on the page using the given search parameters.
-			#~ }
-		#~ }
-
-		#~ NoSuchFrame { SELENIUM NoSuchFrame {
-			#~ A command to switch to a frame could not be satisfied because the frame could not be found.
-			#~ }
-		#~ }
-
-		#~ NoSuchWindow { SELENIUM NoSuchWindow {
-			#~ A command to switch to a window could not be satisfied because the window could not be found.
-			#~ }
-		#~ }
-
-		#~ ScriptTimeout { SELENIUM ScriptTimeout {
-			#~ A script did not complete before its timeout expired.
-			#~ }
-		#~ }
-
-		#~ SessionNotCreated { SELENIUM SessionNotCreated {
-			#~ A new session could not be created.
-			#~ }
-		#~ }
-
-		#~ StaleElementReference { SELENIUM StaleElementReference {
-			#~ A command failed because the referenced element is no longer attached to the DOM.
-			#~ }
-		#~ }
-
-		#~ Timeout { SELENIUM Timeout {
-			#~ An operation did not complete before its timeout expired.
-			#~ }
-		#~ }
-
-		#~ UnableToSetCookie { SELENIUM UnableToSetCookie {
-			#~ A command to set a cookie’s value could not be satisfied.
-			#~ }
-		#~ }
-
-		#~ UnableToCaptureScreen { SELENIUM UnableToCaptureScreen {
-			#~ A screen capture was made impossible.
-			#~ }
-		#~ }
-
-		#~ UnexpectedAlertOpen { SELENIUM UnexpectedAlertOpen {
-			#~ A modal dialog was open, blocking this operation.
-			#~ }
-		#~ }
-
-		#~ UnknownCommand { SELENIUM UnknownCommand {
-			#~ A command could not be executed because the remote end is not aware of it.
-			#~ }
-		#~ }
-
-		#~ UnknownError { SELENIUM UnknownError {
-			#~ An unknown error occurred in the remote end while processing the command.
-			#~ }
-		#~ }
-
-		#~ UnknownMethod { SELENIUM UnknownMethod {
-			#~ The requested command matched a known URL but did not match an method for that URL.
-			#~ }
-		#~ }
-
-		#~ UnsupportedOperation { SELENIUM UnsupportedOperation {
-			#~ Indicates that a command that should have executed properly cannot be supported for some reason.
-			#~ }
-		#~ }
-
